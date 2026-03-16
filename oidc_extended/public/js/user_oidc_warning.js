00bdd8362c16790cc40b430fbf9ea13247e491ce
Mohammed Noureldin <contact@mohammednoureldin.com>
2026-03-17 00:01:59 +0100

    Refactor OIDC warning script to simplify warning display

--- oidc_extended/public/js/user_oidc_warning.js
+++ oidc_extended/public/js/user_oidc_warning.js
@@ -1,41 +1,24 @@
 frappe.ui.form.on("User", {
   refresh(frm) {
-    const is_admin = frm.doc.name === "Administrator";
-    console.log("OIDC warning script running on User:", frm.doc.name);
-
-    if (!is_admin) {
+    if (frm.doc.name !== "Administrator") {
       const warning_msg = __(
-        "Roles and modules of this user are managed by OIDC. Any manual changes made here will be overwritten upon the user's next login."
+        "Roles and module profiles of this user are managed by OIDC. Any manual changes made here will be overwritten upon the user's next login."
       );
 
-      let add_warning_to_wrapper = ($wrapper) => {
-        if ($wrapper && $wrapper.find(".oidc-warning").length === 0) {
-          $(`<div class="alert alert-warning oidc-warning" style="margin-bottom: 10px;">
-              <strong><i class="fa fa-exclamation-triangle"></i> ${warning_msg}</strong>
-          </div>`).prependTo($wrapper);
-        }
-      };
+      frm.set_intro(warning_msg, "orange");
 
-      // Show warning securely within the Roles section
-      let $roles_wrapper = frm.fields_dict.roles ? frm.fields_dict.roles.wrapper : null;
-      let $role_profiles_wrapper = frm.fields_dict.role_profiles ? frm.fields_dict.role_profiles.wrapper : null;
-
-      add_warning_to_wrapper($roles_wrapper);
-      add_warning_to_wrapper($role_profiles_wrapper);
-
-      // Show warning inside Modules section
-      let $modules_wrapper = frm.fields_dict.block_modules ? frm.fields_dict.block_modules.wrapper : null;
-      let $module_profile_wrapper = frm.fields_dict.module_profile ? frm.fields_dict.module_profile.wrapper : null;
-      
-      add_warning_to_wrapper($modules_wrapper);
-      add_warning_to_wrapper($module_profile_wrapper);
-
-      // Disable profiles editing visually
+      // Disable profile editing fields visually
+      if (frm.fields_dict.roles) {
+          frm.set_df_property("roles", "read_only", 1);
+      }
       if (frm.fields_dict.role_profiles) {
           frm.set_df_property("role_profiles", "read_only", 1);
       }
       if (frm.fields_dict.module_profile) {
           frm.set_df_property("module_profile", "read_only", 1);
+      }
+      if (frm.fields_dict.block_modules) {
+          frm.set_df_property("block_modules", "read_only", 1);
       }
     }
   },
