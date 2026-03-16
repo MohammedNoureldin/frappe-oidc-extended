b9ce8f64987b30e94235e3d1d6b999da07cc73f6
Mohammed Noureldin <contact@mohammednoureldin.com>
2026-03-17 00:44:06 +0100

    Remove locking mechanism for module_profile and role_profiles in OIDC warning script

--- oidc_extended/public/js/user_oidc_warning.js
+++ oidc_extended/public/js/user_oidc_warning.js
@@ -22,18 +22,6 @@ frappe.ui.form.on("User", {
             }
         }
 
-        // 2. Simple locking mechanism ONLY for module_profile and role_profiles
-        // We use pure CSS to block clicks and slightly dim them. 
-        // This guarantees Frappe's inner HTML/data representation is completely untouched.
-        ["module_profile", "role_profiles"].forEach(fieldname => {
-            if (frm.fields_dict[fieldname] && frm.fields_dict[fieldname].$wrapper) {
-                frm.fields_dict[fieldname].$wrapper.css({
-                    "pointer-events": "none",
-                    "opacity": "0.6"
-                });
-            }
-        });
-
       }, 800); // 800ms delay guarantees Frappe is completely finished initializing
     }
   },
