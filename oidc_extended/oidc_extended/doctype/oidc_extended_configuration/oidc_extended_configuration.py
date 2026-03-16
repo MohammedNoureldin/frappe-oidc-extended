cc6c4bb476543788bcf56fc99eaa9ee708b0b045
Mohammed Noureldin <contact@mohammednoureldin.com>
2026-03-17 00:02:06 +0100

    Implement validation for duplicate module groups in OIDCExtendedConfiguration

--- oidc_extended/oidc_extended/doctype/oidc_extended_configuration/oidc_extended_configuration.py
+++ oidc_extended/oidc_extended/doctype/oidc_extended_configuration/oidc_extended_configuration.py
@@ -1,8 +1,21 @@
 # Copyright (c) 2023, Mohammed Noureldin and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class OIDCExtendedConfiguration(Document):
-	pass
+	def validate(self):
+		self.validate_duplicate_module_groups()
+		
+	def validate_duplicate_module_groups(self):
+		if not self.group_module_mappings:
+			return
+			
+		seen_groups = set()
+		for row in self.group_module_mappings:
+			if row.group in seen_groups:
+				frappe.throw(
+					frappe._("Row {0}: The group '{1}' is already mapped. Module profiles only support a single mapping per group.").format(row.idx, row.group)
+				)
+			seen_groups.add(row.group)
