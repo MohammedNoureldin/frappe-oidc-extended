d8b35c2b1dcf9f367389a98b18abf3faba9ba04f
Mohammed Noureldin <contact@mohammednoureldin.com>
2026-03-22 02:09:30 +0100

    Remove version assignment from setup.py
    Removed version retrieval from oidc_extended.__init__.py

--- setup.py
+++ setup.py
@@ -3,12 +3,8 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in oidc_extended/__init__.py
-from oidc_extended import __version__ as version
-
 setup(
 	name="oidc_extended",
-	version=version,
 	description="An extension to the ERPNext Social Login authentication method (OIDC) that incorporates new features designed to meet the needs of enterprises.",
 	author="Mohammed Noureldin",
 	author_email="contact@mohammednoureldin.com",
